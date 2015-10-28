289dfb34d1be97f99465f40cc52393efd6a2ada8
Lindsay Wang <tascape@gmail.com>
2015-10-28 13:21:29 -0700

    update dashboard

--- src/main/java/com/tascape/qa/thr/DashboardViewBySuite.java
+++ src/main/java/com/tascape/qa/thr/DashboardViewBySuite.java
@@ -15,22 +15,13 @@
  */
 package com.tascape.qa.thr;
 
-import com.tascape.qa.th.db.SuiteResult;
 import java.io.Serializable;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.naming.NamingException;
-import org.primefaces.model.chart.Axis;
-import org.primefaces.model.chart.AxisType;
-import org.primefaces.model.chart.ChartSeries;
-import org.primefaces.model.chart.HorizontalBarChartModel;
-import org.primefaces.model.chart.LegendPlacement;
-import org.primefaces.model.chart.LinearAxis;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,109 +31,23 @@ import org.slf4j.LoggerFactory;
  */
 @Named
 @RequestScoped
-public class DashboardViewBySuite implements Serializable {
+public class DashboardViewBySuite extends DashboardView implements Serializable {
     private static final Logger LOG = LoggerFactory.getLogger(DashboardViewBySuite.class);
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 5749324382L;
 
     @Inject
     private MySqlBaseBean db;
 
-    private List<Map<String, Object>> results;
-
-    private List<Map<String, Object>> resultsSelected;
-
-    private HorizontalBarChartModel barModel;
-
-    private int chartHeight = 88;
-
-    private int total;
-
-    private int fail;
 
     @PostConstruct
     public void init() {
         try {
-            this.results = this.db.getLatestSuitesResult();
-            this.results.forEach(row -> {
-                row.put("sort", "a");
-            });
+            this.setResults(this.db.getLatestSuitesResult());
         } catch (NamingException | SQLException ex) {
             throw new RuntimeException(ex);
         }
 
-        this.barModel = this.initBarModel();
-    }
-
-    public List<Map<String, Object>> getResults() {
-        return results;
-    }
-
-    public List<Map<String, Object>> getResultsSelected() {
-        return resultsSelected;
-    }
-
-    public void setResultsSelected(List<Map<String, Object>> resultsSelected) {
-        this.resultsSelected = resultsSelected;
-    }
-
-    public HorizontalBarChartModel getBarModel() {
-        return barModel;
-    }
-
-    public int getTotal() {
-        return total;
-    }
-
-    public int getFail() {
-        return fail;
-    }
-
-    public int getChartHeight() {
-        return chartHeight;
-    }
-
-    private HorizontalBarChartModel initBarModel() {
-        HorizontalBarChartModel model = new HorizontalBarChartModel();
-        model.setLegendPosition("n");
-        model.setLegendPlacement(LegendPlacement.OUTSIDEGRID);
-        model.setLegendCols(2);
-        model.setSeriesColors("00ff00, ff0000");
-        model.setStacked(true);
-        model.setBarMargin(0);
-        model.setBarPadding(0);
-        model.setAnimate(false);
-
-        ChartSeries failSeries = new ChartSeries();
-        failSeries.setLabel("FAIL");
-        ChartSeries passSeries = new ChartSeries();
-        passSeries.setLabel("PASS");
-        int f = 0;
-        int t = 0;
-        for (Map<String, Object> result : this.results) {
-            f += Integer.parseInt(result.get(SuiteResult.NUMBER_OF_FAILURE) + "");
-            t += Integer.parseInt(result.get(SuiteResult.NUMBER_OF_TESTS) + "");
-        }
-        LOG.debug("fail {}, total {}", f, t);
-        failSeries.set(" ", f);
-        passSeries.set(" ", t - f);
-        model.addSeries(passSeries);
-        model.addSeries(failSeries);
-
-        this.total = t;
-        this.fail = f;
-
-        Axis xAxis = new LinearAxis("Total Number of Tests");
-        xAxis.setTickAngle(-90);
-        xAxis.setMin(0);
-        if (t <= 28) {
-            xAxis.setTickInterval("1");
-        }
-        xAxis.setMax(t);
-        xAxis.setTickFormat("%" + (t + "").length() + "d");
-        model.getAxes().put(AxisType.X, xAxis);
-
-        this.chartHeight = 86 + (t + "").length() * 7;
-        return model;
+        this.initBarModel();
     }
 }
