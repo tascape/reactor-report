aaf8b592ec7202bbfa4b99f004c4905fa0a9cc69
Linsong Wang <tascape@gmail.com>
2016-11-07 00:29:47 -0800

    fix end time error

--- src/main/java/com/tascape/reactor/report/MySqlBaseBean.java
+++ src/main/java/com/tascape/reactor/report/MySqlBaseBean.java
@@ -143,7 +143,7 @@ public class MySqlBaseBean implements Serializable {
 
     public List<Map<String, Object>> getLatestJobsResult(String project, int weeks) throws NamingException, SQLException {
         long end = System.currentTimeMillis();
-        return this.getLatestJobsResult(project, end - end - 604800000L * weeks, end);
+        return this.getLatestJobsResult(project, end - 604800000L * weeks, end);
     }
 
     public List<Map<String, Object>> getLatestJobsResult(String project, long start, long end) throws NamingException,
