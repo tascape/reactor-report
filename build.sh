d7bb3f732ef3433e3ae1e2056548a114cf886fe1
Linsong Wang <tascape@gmail.com>
2018-01-03 23:31:24 -0800

    set version number

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
-RR_BASE_VERSION=1.2
+RR_BASE_VERSION=$(git describe --abbrev=0 --tags)
+RR_BUILD_NUM=$(git rev-list --count --first-parent ${RR_BASE_VERSION}..HEAD)
+RR_VERSION=${RR_BASE_VERSION}.${RR_BUILD_NUM}
+echo "reactor-report version is ${RR_VERSION}"
 
+mvn versions:set -DnewVersion=${RR_VERSION}
 mvn clean package
 
 docker images
