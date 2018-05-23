d0a6abaf5d7555e1ed8b9f874f2b761011c8763f
Linsong Wang <tascape@users.noreply.github.com>
2018-05-23 03:42:25 -0700

    Update docker-push.sh

--- docker-push.sh
+++ docker-push.sh
@@ -3,8 +3,10 @@
 docker images
 source rr_version
 
+echo "log into tascape (https://hub.docker.com/u/tascape/)"
+docker login -u tascape
+
 echo "push images to tascape"
-docker login
 for IMG in nginx tomee mysql; do
   docker push tascape/reactor-report-$IMG:${RR_VERSION}
   docker push tascape/reactor-report-$IMG:latest
